f7f2ce0bbafd6fface7679566bb86b09a80db971
James Deathe <james.deathe@jadu.net>
2014-08-18 20:30:20 +0100

    Added systemd unit file and helper installation script for running as a service on CoreOS

---
+++ systemd-install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DIR_PATH="$( cd "$( echo "${0%/*}" )"; pwd )"
+if [[ $DIR_PATH == */* ]]; then
+	cd $DIR_PATH
+fi
+
+OPT_SERVICE_NAME_FULL=${SERVICE_NAME_FULL:-apache-php.app-1.1.1@8080.service}
+OPT_SERVICE_NAME_SHORT=$(cut -d '@' -f1 <<< "${OPT_SERVICE_NAME_FULL}")
+
+# Force 
+systemctl stop ${OPT_SERVICE_NAME_FULL}
+docker rm volume-config.${OPT_SERVICE_NAME_SHORT} && \
+docker rm ${OPT_SERVICE_NAME_SHORT}
+
+cp ${OPT_SERVICE_NAME_FULL} /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable /etc/systemd/system/${OPT_SERVICE_NAME_FULL}
+
+echo "WARNING: This may take a while if pulling large container images for the first time..."
+systemctl restart ${OPT_SERVICE_NAME_FULL}
+
+sleep 10
+
+docker logs ${OPT_SERVICE_NAME_SHORT}
