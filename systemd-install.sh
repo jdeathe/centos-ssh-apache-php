8cb2348c14814b1b11e2337f7781c9774cea924a
James Deathe <james.deathe@jadu.co.uk>
2016-01-18 16:44:30 +0000

    ISSUE 55: Updated BASH scripts to be consistent with fcgi version.

--- systemd-install.sh
+++ systemd-install.sh
@@ -1,42 +1,51 @@
 #!/usr/bin/env bash
 
-DIR_PATH="$( if [ "$( echo "${0%/*}" )" != "$( echo "${0}" )" ] ; then cd "$( echo "${0%/*}" )"; fi; pwd )"
-if [[ $DIR_PATH == */* ]] && [[ $DIR_PATH != "$( pwd )" ]] ; then
-	cd $DIR_PATH
+# Change working directory
+DIR_PATH="$( if [[ $( echo "${0%/*}" ) != $( echo "${0}" ) ]]; then cd "$( echo "${0%/*}" )"; fi; pwd )"
+if [[ ${DIR_PATH} == */* ]] && [[ ${DIR_PATH} != $( pwd ) ]]; then
+	cd ${DIR_PATH}
 fi
 
 have_docker_container_name ()
 {
 	local NAME=$1
 
-	if [[ -n $(docker ps -a | awk -v pattern="^${NAME}$" '$NF ~ pattern { print $NF; }') ]]; then
-		return 0
-	else
+	if [[ -z ${NAME} ]]; then
 		return 1
 	fi
+
+	if [[ -n $(docker ps -a | awk -v pattern="^${NAME}$" '$NF ~ pattern { print $NF; }') ]]; then
+		return 0
+	fi
+
+	return 1
 }
 
 is_docker_container_name_running ()
 {
 	local NAME=$1
 
-	if [[ -n $(docker ps | awk -v pattern="^${NAME}$" '$NF ~ pattern { print $NF; }') ]]; then
-		return 0
-	else
+	if [[ -z ${NAME} ]]; then
 		return 1
 	fi
+
+	if [[ -n $(docker ps | awk -v pattern="^${NAME}$" '$NF ~ pattern { print $NF; }') ]]; then
+		return 0
+	fi
+
+	return 1
 }
 
 remove_docker_container_name ()
 {
 	local NAME=$1
 
-	if have_docker_container_name ${NAME} ; then
-		if is_docker_container_name_running ${NAME} ; then
-			echo Stopping container ${NAME}
+	if have_docker_container_name ${NAME}; then
+		if is_docker_container_name_running ${NAME}; then
+			echo "Stopping container ${NAME}"
 			(docker stop ${NAME})
 		fi
-		echo Removing container ${NAME}
+		echo "Removing container ${NAME}"
 		(docker rm ${NAME})
 	fi
 }
@@ -45,7 +54,7 @@ loading_counter ()
 {
 	local COUNTER=${1:-10}
 
-	while [ ${COUNTER} -ge 1 ]; do
+	while [[ ${COUNTER} -ge 1 ]]; do
 		echo -ne "Loading in: ${COUNTER} \r"
 		sleep 1
 		COUNTER=$[${COUNTER}-1]
@@ -58,11 +67,11 @@ OPT_SERVICE_NAME_SHORT=$(cut -d '@' -f1 <<< "${OPT_SERVICE_NAME_FULL}")
 # Add required configuration directories
 mkdir -p /etc/services-config/${OPT_SERVICE_NAME_SHORT}/{httpd,supervisor,ssl/{certs,private}}
 
-if [[ ! -n $(find /etc/services-config/${OPT_SERVICE_NAME_SHORT}/supervisor -maxdepth 1 -type f) ]]; then
+if [[ -z $(find /etc/services-config/${OPT_SERVICE_NAME_SHORT}/supervisor -maxdepth 1 -type f) ]]; then
 	cp -R etc/services-config/supervisor /etc/services-config/${OPT_SERVICE_NAME_SHORT}/
 fi
 
-if [[ ! -n $(find /etc/services-config/${OPT_SERVICE_NAME_SHORT}/httpd -maxdepth 1 -type f) ]]; then
+if [[ -z $(find /etc/services-config/${OPT_SERVICE_NAME_SHORT}/httpd -maxdepth 1 -type f) ]]; then
 	cp -R etc/services-config/httpd /etc/services-config/${OPT_SERVICE_NAME_SHORT}/
 fi
 
@@ -75,7 +84,7 @@ sudo cp ${OPT_SERVICE_NAME_FULL} /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable /etc/systemd/system/${OPT_SERVICE_NAME_FULL}
 
-echo "WARNING: This may take a while if pulling large container images for the first time..."
+echo "WARNING: This may take a while if pulling large container images for the first time."
 sudo systemctl restart ${OPT_SERVICE_NAME_FULL}
 
 loading_counter 10
