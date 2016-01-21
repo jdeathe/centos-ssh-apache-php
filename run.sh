9fb3399dbfe04509c03d3c87829cd5b877f9e75e
James Deathe <james.deathe@jadu.co.uk>
2016-01-21 22:20:02 +0000

    ISSUE 64: Order Environment variables alphabetically.

--- run.sh
+++ run.sh
@@ -140,10 +140,10 @@ docker run \
 	--env "SERVICE_UNIT_LOCAL_ID=${SERVICE_UNIT_LOCAL_ID}" \
 	--env "SERVICE_UNIT_INSTANCE=${SERVICE_UNIT_INSTANCE}" \
 	--env "APACHE_EXTENDED_STATUS_ENABLED=${APACHE_EXTENDED_STATUS_ENABLED}" \
-	--env "APACHE_SERVER_ALIAS=${APACHE_SERVER_ALIAS}" \
-	--env "APACHE_SERVER_NAME=${APACHE_SERVER_NAME}" \
 	--env "APACHE_LOAD_MODULES=${APACHE_LOAD_MODULES}" \
 	--env "APACHE_MOD_SSL_ENABLED=${APACHE_MOD_SSL_ENABLED}" \
+	--env "APACHE_SERVER_ALIAS=${APACHE_SERVER_ALIAS}" \
+	--env "APACHE_SERVER_NAME=${APACHE_SERVER_NAME}" \
 	--env "APP_HOME_DIR=${APP_HOME_DIR}" \
 	--env "DATE_TIMEZONE=${DATE_TIMEZONE}" \
 	--env "HTTPD=${HTTPD}" \
@@ -170,10 +170,10 @@ docker run \
 # 	--env "SERVICE_UNIT_LOCAL_ID=1" \
 # 	--env "SERVICE_UNIT_INSTANCE=1" \
 # 	--env "APACHE_EXTENDED_STATUS_ENABLED=true"
-# 	--env "APACHE_SERVER_ALIAS=app-1 www.app-1 www.app-1.local" \
-# 	--env "APACHE_SERVER_NAME=app-1.local" \
 # 	--env "APACHE_LOAD_MODULES=${APACHE_LOAD_MODULES} rewrite_module" \
 # 	--env "APACHE_MOD_SSL_ENABLED=false" \
+# 	--env "APACHE_SERVER_ALIAS=app-1 www.app-1 www.app-1.local" \
+# 	--env "APACHE_SERVER_NAME=app-1.local" \
 # 	--env "APP_HOME_DIR=/var/www/app-1" \
 # 	--env "DATE_TIMEZONE=Europe/London" \
 # 	--env "HTTPD=/usr/sbin/httpd.worker" \
