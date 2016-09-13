b67d99efe1b3722e9aaf531db3068d76f4565703
James Deathe <james.deathe@gmail.com>
2016-09-13 10:51:53 +0100

    ISSUE 185: Adds method to prevent exposed ports being published when installing via scmi or systemd template unit-file.

--- opt/scmi/default.sh
+++ opt/scmi/default.sh
@@ -1,23 +1,26 @@
 
 # If gawk is available handle incrementing the docker host port for instances
 DOCKER_PUBLISH=
-if command -v gawk &> /dev/null \
-	&& [[ -n $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_80}") ]]; then
-	printf -v \
-		DOCKER_PUBLISH \
-		-- '%s --publish %s%s:80' \
-		"${DOCKER_PUBLISH}" \
-		"$(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_PORT_MAP_TCP_80}")" \
-		"$(( $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_80}") + $(gawk 'match($0, /^.+\.([0-9]+)\.([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_NAME}") - 1 ))"
-else
-	printf -v \
-		DOCKER_PUBLISH \
-		-- '%s --publish %s:80' \
-		"${DOCKER_PUBLISH}" \
-		"${DOCKER_PORT_MAP_TCP_80}"
+if [[ ${DOCKER_PORT_MAP_TCP_80} != NULL ]]; then
+	if command -v gawk &> /dev/null \
+		&& [[ -n $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_80}") ]]; then
+		printf -v \
+			DOCKER_PUBLISH \
+			-- '%s --publish %s%s:80' \
+			"${DOCKER_PUBLISH}" \
+			"$(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_PORT_MAP_TCP_80}")" \
+			"$(( $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_80}") + $(gawk 'match($0, /^.+\.([0-9]+)\.([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_NAME}") - 1 ))"
+	else
+		printf -v \
+			DOCKER_PUBLISH \
+			-- '%s --publish %s:80' \
+			"${DOCKER_PUBLISH}" \
+			"${DOCKER_PORT_MAP_TCP_80}"
+	fi
 fi
 
-if [[ ${APACHE_MOD_SSL_ENABLED} == true ]]; then
+if [[ ${DOCKER_PORT_MAP_TCP_443} != NULL ]] \
+	&& [[ ${APACHE_MOD_SSL_ENABLED} == true ]]; then
 	if command -v gawk &> /dev/null \
 		&& [[ -n $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_443}") ]]; then
 		printf -v \
@@ -35,25 +38,26 @@ if [[ ${APACHE_MOD_SSL_ENABLED} == true ]]; then
 	fi
 fi
 
-if command -v gawk &> /dev/null \
-	&& [[ -n $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}") ]]; then
-	printf -v \
-		DOCKER_PUBLISH \
-		-- '%s --publish %s%s:8443' \
-		"${DOCKER_PUBLISH}" \
-		"$(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}")" \
-		"$(( $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}") + $(gawk 'match($0, /^.+\.([0-9]+)\.([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_NAME}") - 1 ))"
-else
-	printf -v \
-		DOCKER_PUBLISH \
-		-- '%s --publish %s:8443' \
-		"${DOCKER_PUBLISH}" \
-		"${DOCKER_PORT_MAP_TCP_8443}"
+if [[ ${DOCKER_PORT_MAP_TCP_8443} != NULL ]]; then
+	if command -v gawk &> /dev/null \
+		&& [[ -n $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}") ]]; then
+		printf -v \
+			DOCKER_PUBLISH \
+			-- '%s --publish %s%s:8443' \
+			"${DOCKER_PUBLISH}" \
+			"$(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}")" \
+			"$(( $(gawk 'match($0, /^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)?([0-9]+)$/, matches) { print matches[2]; }' <<< "${DOCKER_PORT_MAP_TCP_8443}") + $(gawk 'match($0, /^.+\.([0-9]+)\.([0-9]+)$/, matches) { print matches[1]; }' <<< "${DOCKER_NAME}") - 1 ))"
+	else
+		printf -v \
+			DOCKER_PUBLISH \
+			-- '%s --publish %s:8443' \
+			"${DOCKER_PUBLISH}" \
+			"${DOCKER_PORT_MAP_TCP_8443}"
+	fi
 fi
 
 # Common parameters of create and run targets
 DOCKER_CONTAINER_PARAMETERS="--name ${DOCKER_NAME} \
-${DOCKER_PUBLISH} \
 --restart ${DOCKER_RESTART_POLICY} \
 --env \"APACHE_CONTENT_ROOT=${APACHE_CONTENT_ROOT}\" \
 --env \"APACHE_CUSTOM_LOG_FORMAT=${APACHE_CUSTOM_LOG_FORMAT}\" \
@@ -72,4 +76,5 @@ ${DOCKER_PUBLISH} \
 --env \"APACHE_SERVER_ALIAS=${APACHE_SERVER_ALIAS}\" \
 --env \"APACHE_SERVER_NAME=${APACHE_SERVER_NAME}\" \
 --env \"APACHE_SYSTEM_USER=${APACHE_SYSTEM_USER}\" \
---env \"PHP_OPTIONS_DATE_TIMEZONE=${PHP_OPTIONS_DATE_TIMEZONE}\""
+--env \"PHP_OPTIONS_DATE_TIMEZONE=${PHP_OPTIONS_DATE_TIMEZONE}\" \
+${DOCKER_PUBLISH}"
